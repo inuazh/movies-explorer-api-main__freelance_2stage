1625f7b286067a3c83f8b9789a5305e2ea91d70a
Ramazan <108758151+inuazh@users.noreply.github.com>
2024-01-27 15:42:54 +0300

    1

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+Адрес репозитория: https://github.com/filippovdo/movies-explorer-api
+
+
+
+## Ссылки на проект
+
+IP 
+
+Frontend 
+Backend 
+
+Репозиторий frontend: https://github.com/filippovdo/movies-explorer-frontend
---
+++ routes/index.js
@@ -0,0 +1,46 @@
+const router = require('express').Router();
+
+
+const { celebrate } = require('celebrate');
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('../middlewares/logger');
+
+const { login, createUser } = require('../controllers/users');
+const auth = require('../middlewares/auth');
+const { rateLimiter } = require('../middlewares/rateLimiter');
+const centralizedErrorHandler = require('../middlewares/centralizedErrorHandler');
+const NotFoundError = require('../utils/errors/not-found-err');
+
+
+
+const { JoiBodyEmailPassword, JoiBodyEmailPasswordName } = require('../utils/validationConstants');
+
+router.use(requestLogger);
+
+router.use(rateLimiter);
+
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+
+
+router.post('/signin', celebrate(JoiBodyEmailPassword), login);
+router.post('/signup', celebrate(JoiBodyEmailPasswordName), createUser);
+
+router.use(auth);
+
+router.use('/users', require('./users'));
+router.use('/movies', require('./movies'));
+
+router.use('*', (req, res, next) => next(new NotFoundError('Страница не найдена')));
+
+router.use(errorLogger);
+
+router.use(errors());
+
+router.use(centralizedErrorHandler);
+
+module.exports = router;
